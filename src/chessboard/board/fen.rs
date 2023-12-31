e6c06da5dbabc8dcafd9c6107fec5d145fb2cffd
Giffi <miklas.karjalainen@gmail.com>
2023-12-31 14:47:19 +0200

    implement to_fen for board

--- src/chessboard/board/fen.rs
+++ src/chessboard/board/fen.rs
@@ -62,22 +62,34 @@ impl ChessBoard {
                 self.en_passant = BoardHelper::text_to_square(en_passant);     
             }
         }
+
+        // Parse half move
+        if let Some(half_move) = args.pop() {
+            if let Ok(parsed) = half_move.parse::<u8>() {
+                self.half_move = parsed;     
+            }
+        }
+
+        // Parse full move
+        if let Some(full_move) = args.pop() {
+            if let Ok(parsed) = full_move.parse::<u16>() {
+                self.full_move = parsed;     
+            }
+        }
         
     }
 
-    pub fn to_fen(board: ChessBoard) -> String {
-        todo!();
+    pub fn to_fen(&self) -> String {
         let mut fen = String::new();
         
         let mut empty_counter = 0u8;
 
         // Board
-        for y in 0..8 {
+        for y in (0..8).rev() {
             for x in 0..8 {
                 let idx = y * 8 + x;
-                // let piece = board.get_piece(idx).unwrap();
+                let piece = self.get_piece(idx);
 
-                /*
                 if piece.is_none() {
                     empty_counter += 1;
                 }
@@ -89,7 +101,6 @@ impl ChessBoard {
                     }
                     fen.push(piece_char);
                 }
-                */
             }
             if empty_counter != 0 {
                 fen.push((('0' as u8) + empty_counter) as char);
@@ -101,25 +112,38 @@ impl ChessBoard {
 
         // Turn
         fen.push(' ');
-        fen.push(if board.get_turn() == PieceColor::White {'w'} else {'b'});
-
-        // Todo: Castling
-        fen.push_str(" KQkq ");
-
-        // En passant sqaure
-        fen.push('-');
+        fen.push(if self.get_turn() == PieceColor::White {'w'} else {'b'});
         
-        /*
-        if board.en_passant_square == -1 {
-            
+        // Castling rights
+        fen.push(' ');
+        if self.castling_rights[0] || self.castling_rights[1] || self.castling_rights[2] || self.castling_rights[3] {
+            if self.castling_rights[0] { fen.push('K'); }
+            if self.castling_rights[1] { fen.push('Q'); }
+            if self.castling_rights[2] { fen.push('k'); }
+            if self.castling_rights[3] { fen.push('q'); }
         }
         else {
-            let (file, rank) = BoardHelper::square_to_chars(board.en_passant_square);
+            fen.push('-');
+        }
+
+        // En passant square
+        fen.push(' ');
+        if self.en_passant != -1 {
+            let (file, rank) = BoardHelper::square_to_chars(self.en_passant);
             fen.push(file);
             fen.push(rank);
         }
-        */
-        
+        else {
+            fen.push('-');
+        }
+
+        // Half & Full -moves
+        fen.push(' ');
+        fen.push_str(&self.half_move.to_string());
+        fen.push(' ');
+        fen.push_str(&self.full_move.to_string());
+
+
         fen
     }
 
@@ -133,10 +157,8 @@ mod tests {
 
     #[test]
     fn test_parse_fen_basic1() {
-        const TEST_PROMOTION_FEN: &str = "4k3/2P5/4K3/8/8/8/5p2/8 b - - 0 1";
-
         let mut board = ChessBoard::new();
-        board.parse_fen(TEST_PROMOTION_FEN);
+        board.parse_fen("4k3/2P5/4K3/8/8/8/5p2/8 b - - 0 1");
 
         assert_eq!(board.get_turn(), PieceColor::Black);
 
@@ -147,11 +169,24 @@ mod tests {
 
     #[test]
     fn test_parse_fen_en_passant() {
-        const TEST_EN_PASSANT_FEN: &str = "4k3/8/8/5Pp1/8/8/8/4K3 w - g6 0 1";
-
         let mut board = ChessBoard::new();
-        board.parse_fen(TEST_EN_PASSANT_FEN);
-
+        board.parse_fen("4k3/8/8/5Pp1/8/8/8/4K3 w - g6 0 1");
         assert_eq!(board.en_passant, BoardHelper::text_to_square("g6"));
+    }
+
+    #[test]
+    fn test_parse_fen_half_and_full_moves() {
+        let mut board = ChessBoard::new();
+        board.parse_fen("8/4k3/3p1p2/2pP1Pp1/2P1K1P1/8/8/8 w - - 69 420");
+        assert_eq!(board.half_move, 69);
+        assert_eq!(board.full_move, 420);
+    }
+
+    #[test]
+    fn test_to_fen_startpos() {
+        let mut board = ChessBoard::new();
+        board.parse_fen(STARTPOS_FEN);
+        board.make_move_uci("e2e4");
+        assert_eq!(board.to_fen(), "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
     }
 }
