f8256c88390e3c2a43527ace730f7b80851fab89
Giffi <giffidev@gmail.com>
2024-06-21 18:49:02 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,18 @@
 #![doc = include_str!("../README.md")]
 
 mod bitschess;
-
 mod piece;
-pub use piece::*;
-
 mod chess_move;
-pub use chess_move::*;
-
-
 mod board_helper;
-pub use board_helper::*;
-
 pub mod prelude {
+    pub use super::board_helper::*;
+    
     pub use super::bitschess::board::*;
     pub use super::bitschess::bitboard::*;
     pub use super::bitschess::board::fen::*;
-
+    
+    pub use super::chess_move::*;
+    
     pub use super::piece::*;
 }
 
