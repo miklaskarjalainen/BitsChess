e621869ca4a686ce851d18bcda72191b42b4f191
Giffi <giffidev@gmail.com>
2024-06-21 20:08:44 +0300

    fix doc tests not passing

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,22 @@
 #![doc = include_str!("../README.md")]
 
 mod bitschess;
-mod piece;
-mod chess_move;
+
 mod board_helper;
+pub use board_helper::*;
+
+mod piece;
+pub use piece::*;
+
+mod chess_move;
+pub use chess_move::*;
+
 pub mod prelude {
     pub use super::board_helper::*;
-    
     pub use super::bitschess::board::*;
-    pub use super::bitschess::bitboard::*;
     pub use super::bitschess::board::fen::*;
-    
+    pub use super::bitschess::bitboard::*;
     pub use super::chess_move::*;
-    
     pub use super::piece::*;
 }
 
